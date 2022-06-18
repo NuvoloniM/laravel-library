05a5f8fd8a5db7a9b0095e457c53a609f62bb8dc
NuvoloniM <nuvoloni.m@gmail.com>
2022-06-18 16:13:47 +0200

    Scaffolding auth + rotte

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::middleware('auth')
+->prefix('admin')
+->name('admin.')
+->namespace('Admin')
+->group(function () {
+    Route::get('/', 'HomeController@index')->name('home');
 });
+
+
